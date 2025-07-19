19bd71329366747599cfe3acb0d4f2c5e2ad1099
Krishna <krishna.narayana5b@gmail.com>
2025-07-19 17:22:25 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative-AI-for-medical-reports',
+    version='0.0.0',
+    packages= find_packages(),
+    install_requires=[],
+    author='Krishna',
+    author_email='krishna.narayana5b@gmail.com'
+)
